3e438c67a7a5fb6145c6581f34871534df994b63
nikoagge <nikoagge@gmail.com>
2023-05-15 19:20:31 +0300

    Implemented Models for the app

--- GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -10,6 +10,9 @@ import RxSwift
 import RxCocoa
 
 final class NewsListTableViewController: UITableViewController {
+    private let disposeBag = DisposeBag()
+    private var articles = [Article]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,15 +32,16 @@ private extension NewsListTableViewController {
         Observable.just(url)
             .flatMap { url -> Observable<Data> in
                 let request = URLRequest(url: url)
-                return return URLSession.shared.rx.data(request: request)
+                return URLSession.shared.rx.data(request: request)
             }.map { data -> [Article]? in
-                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+                return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
             }.subscribe(onNext: { [weak self] articles in
                 if let articles = articles {
                     DispatchQueue.main.async {
+                        self?.articles = articles
                         self?.tableView.reloadData()
                     }
                 }
-            })
+            }).disposed(by: disposeBag)
     }
 }
